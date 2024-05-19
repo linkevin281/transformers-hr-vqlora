a6f551802bc33683befce819011bf42f88a2a846
linkevin281 <linkevin281@gmail.com>
2024-05-19 16:27:45 -0400

    Update trainer compute_loss() function to hierarchical loss

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -3270,6 +3270,26 @@ class Trainer:
         else:
             labels = None
         outputs = model(**inputs)
+        
+        ### MODIFIED CODE ### 
+        
+        criterion = nn.MSELoss()
+
+        latent_loss_weight = 0.25
+
+        # mse_sum = 0
+        # mse_n = 0
+        latent_loss = outputs["loss"]
+        if "logits" in outputs:
+            print(f' logits: {outputs["logits"]} type: {type(outputs["logits"])}')
+        print(f' output keys: {outputs.keys()}, model type: {model}')
+        
+        recon_loss = criterion(outputs["logits"], inputs)
+        latent_loss = latent_loss.mean()
+        loss = recon_loss + (latent_loss_weight * latent_loss)
+
+        ### MODIFIED CODE ### 
+        
         # Save past state if it exists
         # TODO: this needs to be fixed and made cleaner later.
         if self.args.past_index >= 0:
